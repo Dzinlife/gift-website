7a35defe70e8bbffae8b342d7fc7e952d4b1d844
Dzinlife <dzinlife@me.com>
2019-02-11 16:17:25 +0800

    copy index.html to 200.html

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 const isProd = process.env.NODE_ENV === 'production'
+const WebpackShellPlugin = require('webpack-shell-plugin')
+const path = require('path')
 
 module.exports = {
   outputDir: 'docs',
-  indexPath: '200.html',
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'development') {
       config
         .output
         .filename('[name].[hash].js')
         .end()
+    } else {
     }
+    config.plugin('200.html').use(WebpackShellPlugin, [
+      {
+        onBuildExit: [
+          'echo "copying index.html to 200.html"',
+          'cp docs/index.html docs/200.html'
+        ]
+      }
+    ])
   }
 }
